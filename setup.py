2fc55dd2cadc4f04f9a04dcc9192a03f98904dca
Panayotis Vryonis <vrypan@gmail.com>
2012-11-04 10:34:04 +0200

    setup.py code cleanup

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup
-# from distutils.core import setup
 import os, sys
-
-data_files_list = []
-rootdir = 'bucket3/_themes'
-
-for root, subFolders, files in os.walk(rootdir):
-    #data_files_list.append(root)
-    for file in files:
-        data_files_list.append(os.path.join(root,file))
 
 setup(
     name='bucket3',
@@ -18,8 +9,6 @@ setup(
     packages=['bucket3'],
     scripts=['bin/bucket3'],
     package_dir={'bucket3': 'bucket3'},
-    # package_data = { 'bucket3' : data_files_list }, 
-    # data_files = data_files_list,
     include_package_data=True,
     url='http://pypi.python.org/pypi/Bucket3/',
     license='MIT-LICENSE.txt',
